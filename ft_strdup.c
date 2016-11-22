d44ea81db56eb88560f1d24c2bba24b2fd6f7868
root <root@localhost.localdomain>
2016-11-22 18:31:08 +0100

    strcpy strlen strdup + .h

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,13 +1,13 @@
-/* #include "libft.h" */
+#include "libft.h"
+#include <stdlib.h>
 
-char	*ft_strdup(const char *s)
+char	*ft_strdup(const char *str)
 {
 	char	*cpy;
 
-	if ((cpy = (char *)malloc(sizeof(*cpy) * (ft_strlen(s) + 1))))
+	if ((cpy = (char *)malloc(sizeof(*cpy) * (ft_strlen(str) + 1))))
 	{
-		ft_memcpy(cpy, s, ft_strlen(s));
-		cpy[ft_strlen(s)] = '\0';
+		ft_strcpy(cpy, str);
 		return (cpy);
 	}
 	else
---
+++ libft.h
@@ -0,0 +1,12 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+
+char	*ft_strcpy(char *dest, const char *src);
+
+char	*ft_strdup(const char *str);
+
+size_t	ft_strlen(const char *str);
+
+#endif
