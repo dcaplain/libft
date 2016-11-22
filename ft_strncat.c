ba4399768ba2de77b96aa7941f7ff05bffdda021
root <root@localhost.localdomain>
2016-11-22 19:24:11 +0100

    strncat, modif strncpy + h

---
+++ ft_strncat.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+char	*ft_strncat(char *dest, const char *src, size_t n)
+{
+	while (dest)
+		dest++;
+	while (src && n > 0)
+	{
+		*(dest++) = *(src++);
+		n--;
+	}
+	dest = '\0';
+	return (dest);
+}
--- ft_strncpy.c
+++ ft_strncpy.c
@@ -10,6 +10,5 @@ char	*strncpy(char *dest, const char *src, size_t n)
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
